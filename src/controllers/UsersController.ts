4194765f5852d75efe1f7e57679f98238ca19069
gustavoo151 <josegustavooliveira121@gmail.com>
2025-03-04 22:57:51 -0300

    fix: correct import path casing for UsersService for consistency

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { UsersService } from "../services/usersService";
+import { UsersService } from "../services/UsersService";
 
 class UsersController {
   async create(request: Request, response: Response) {
