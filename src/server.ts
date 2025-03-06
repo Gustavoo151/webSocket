360879f29fedac77348094d74d504ace37e0ad39
gustavoo151 <josegustavooliveira121@gmail.com>
2025-03-05 23:12:15 -0300

    feat: configure server to use environment variables for host and port

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import { http } from "./http";
 import "./websocket/client";
 import "./websocket/admin";
+import "dotenv/config";
 
-http.listen(3333, () => console.log("Server is running on port 3333"));
+const PORT = Number(process.env.PORT) || 3333;
+const HOST = process.env.HOST || "0.0.0.0";
+
+http.listen(PORT, HOST, () =>
+  console.log(`Server is running on http://${HOST}:${PORT}`)
+);
