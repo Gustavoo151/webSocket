d72625b3285a6f0fc5bdf8d86b7b9334193429f9
gustavoo151 <josegustavooliveira121@gmail.com>
2025-04-21 02:49:25 -0300

    feat: extend IRepository interface with FindOptions and find method for entity retrieval

--- orm-framework/src/core/interfaces/IRepository.ts
+++ orm-framework/src/core/interfaces/IRepository.ts
@@ -1,3 +1,15 @@
+/**
+ * Interface genérica para repositórios do ORM
+ * Define operações padrão de acesso e manipulção de dados que todos os repositórios dev
+ */
+
+export interface FindOptions {
+  skip?: number;
+  take?: number;
+  order?: Record<string, "ASC" | "DESC">;
+  relations?: string[];
+}
+
 export interface IRepository<Entity, IdType = any> {
   /**
    * Busca uma entidade pelo seu ID
@@ -6,4 +18,15 @@ export interface IRepository<Entity, IdType = any> {
    * @retuns Promisse que resolve para entidade encontrada ou null
    */
   findById(id: IdType, relations?: string[]): Promise<Entity | null>;
+
+  /**
+   * Busca entidades com base em critérios específicos
+   * @param criteria Objetos com condições para filtrar as entidades
+   * @param options Opções adicionais como ordenação, paginação, etc.
+   * @returns Promise que resolve para um array de entidades
+   */
+  find(
+    criteria?: Record<string, any>,
+    options?: FindOptions
+  ): Promise<Entity[]>;
 }
